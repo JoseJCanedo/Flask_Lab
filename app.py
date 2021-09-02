288c3d6150b609c393a484e08a519e500653cb45
JoseJCanedo <canedo_jose@columbusstate.edu>
2021-09-02 03:21:08 +0000

    app

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask, render_template       
+app = Flask(__name__)          
+
+@app.route("/")                
+def hello():                    
+    return render_template("index.html")     
+
+@app.route("/<name>")            
+def hello_name(name):           
+    return "Hello "+ name          
+
+if __name__ == "__main__":     
+    app.run(debug=True)         
