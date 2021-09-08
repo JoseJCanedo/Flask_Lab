dcae5ef853647536254823af466748cde7166e21
JoseJCanedo <canedo_jose@columbusstate.edu>
2021-09-08 04:10:05 +0000

    s

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")                
 def hello():                    
-    return render_template("index.html")     
+    return render_template("index.html")
+
+@app.route("/about")                
+def about():                    
+    return render_template("about.html")       
 
 @app.route("/<name>")            
 def hello_name(name):           
